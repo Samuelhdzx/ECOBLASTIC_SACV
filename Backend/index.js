42312d95598603bd45b5ac037832b37f26083552
Samy <hernandezbarriossamuel@gmail.com>
2025-02-05 15:03:47 -0600

    O

--- Backend/index.js
+++ Backend/index.js
@@ -3,6 +3,9 @@ import app from "./app.js";
 const port = process.env.PORT || 9000;
 
 // Create the HTTP server
+
+
+
 const server = app.listen(port, () => console.log("Server listening to", port));
 
 // Remove WebSocket integration from index.js
