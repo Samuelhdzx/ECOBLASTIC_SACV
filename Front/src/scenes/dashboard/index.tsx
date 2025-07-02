f117d692d6809ded563077fbf71d9ff358431997
Samy <hernandezbarriossamuel@gmail.com>
2025-07-01 18:31:29 -0600

    espero ahora shiii

--- Front/src/scenes/dashboard/index.tsx
+++ Front/src/scenes/dashboard/index.tsx
@@ -2,10 +2,10 @@
 import { Box, useMediaQuery, Typography } from '@mui/material';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import Row1 from './Row1';
-import Row2 from './Row2';
-import Row3 from './Row3';
-import Row4 from './Row4';
+// import Row1 from './Row1';
+// import Row2 from './Row2';
+// import Row3 from './Row3';
+// import Row4 from './Row4';
 
 // Grid layouts
 const gridTemplateLargeScreens = `
@@ -101,7 +101,6 @@ const Dashboard = () => {
 
         <Box
           width="100%"
-          
           display="grid"
           gap="1.5rem"
           sx={{
@@ -112,10 +111,13 @@ const Dashboard = () => {
               : gridTemplateSmallScreens,
           }}
         >
-          <Row1 />
+          {/* <Row1 />
           <Row2 />
           <Row3 />
-          <Row4 />
+          <Row4 /> */}
+          <Typography variant="h5" color="white">
+            Dashboard funcionando (prueba)
+          </Typography>
         </Box>
       </Box>
     </Box>
