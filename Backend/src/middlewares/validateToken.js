8ce5ed4fddeedb905dc5f90eb25f44c0ac2f518b
Samy <hernandezbarriossamuel@gmail.com>
2025-07-07 13:35:13 -0600

    ECOBLASTICotravez15

--- Backend/src/middlewares/validateToken.js
+++ Backend/src/middlewares/validateToken.js
@@ -2,7 +2,16 @@ import jwt from "jsonwebtoken";
 import { TOKEN_SECRET } from "../../config.js";
 
 export const validateToken = (req, res, next) => {
-    const { token } = req.cookies;
+    // Buscar en el header Authorization
+    let token = null;
+    const authHeader = req.headers['authorization'];
+    if (authHeader && authHeader.startsWith('Bearer ')) {
+        token = authHeader.split(' ')[1];
+    }
+    // Si no está en el header, buscar en cookies
+    if (!token && req.cookies) {
+        token = req.cookies.token;
+    }
     if (!token) return res.status(401).json({ message: "Sin Autorizacion" });
     
     jwt.verify(token, TOKEN_SECRET, (err, user) => {
