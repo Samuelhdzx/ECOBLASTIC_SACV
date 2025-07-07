47d9a7681b0c97bb43f18a7433bce37ef2e82baa
Samy <hernandezbarriossamuel@gmail.com>
2025-07-07 11:45:09 -0600

    ECOBLASTICotravez6

--- Front/src/Pages/Help.tsx
+++ Front/src/Pages/Help.tsx
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion';
 import video1 from '../../public/video/videouno.mp4';
 import video2 from '../../public/video/videodos.mp4';
 import video3 from '../../public/video/videotres.mp4';
-import './help.css';
+import './Help.css';
 
 interface FAQ {
   question: string;
